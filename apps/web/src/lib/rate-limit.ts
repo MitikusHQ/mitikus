aa112fd73ace02e8a9dbd992888c3555e4a86467
Borja Prieto Valera <borjaprietomark82@gmail.com>
2026-08-20 17:24:21 +0200

    feat: add Upstash ratelimit singleton (fail-open when env not set)

---
+++ apps/web/src/lib/rate-limit.ts
@@ -0,0 +1,15 @@
+import { Ratelimit } from '@upstash/ratelimit'
+import { Redis } from '@upstash/redis'
+
+export const ratelimit =
+  process.env.UPSTASH_REDIS_REST_URL && process.env.UPSTASH_REDIS_REST_TOKEN
+    ? new Ratelimit({
+        redis: new Redis({
+          url: process.env.UPSTASH_REDIS_REST_URL,
+          token: process.env.UPSTASH_REDIS_REST_TOKEN,
+        }),
+        limiter: Ratelimit.slidingWindow(60, '60 s'),
+        analytics: false,
+        prefix: 'mitikus_rl',
+      })
+    : null
